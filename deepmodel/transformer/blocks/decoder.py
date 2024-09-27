7e4824bbf8f156850bcbd3460a0e6a15a7f355b5
trungviet17 <theseventeengv@gmail.com>
2024-09-27 22:03:14 +0700

    update encoder and decoder transformer

--- deepmodel/transformer/blocks/decoder.py
+++ deepmodel/transformer/blocks/decoder.py
@@ -1,39 +1,58 @@
-import torch 
 import torch.nn as nn 
 import math 
-from layer.attention import MultiHeadAttention
+
+from layer.attention import MultiHeadAttention 
+from embedding.positional_emdbedding import PositionalEncoding
 from layer.pointwise_feed_forward import Position_wise_feed_forward
 
 
-class EncoderLayer(nn.Module): 
+class DecoderLayer(nn.Module): 
 
-    def __init__(self, d_model, num_heads, d_ff, dropout):
-        super(EncoderLayer, self).__init__()
+    def __init__(self, d_model, num_head, d_ff, dropout): 
+        super(DecoderLayer, self).__init__()
+        self.masked_multi_att = MultiHeadAttention(d_model, num_head)
+        self.multi_att = MultiHeadAttention(d_model, num_head)
 
-        self.multi_att = MultiHeadAttention(d_model, num_heads)
-        self.feedforward = Position_wise_feed_forward(d_model, d_ff)
-
-        # setup norm and dropout 
         self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.feedforward = Position_wise_feed_forward(d_model, d_ff)
         self.dropout = nn.Dropout(dropout)
 
 
 
-    def forward(self, x, mask ): 
-        att_ouput = self.multi_att(x, mask )
-        x = self.norm1(x + self.dropout(att_ouput))
-        ff = self.feedforward(x)
-        return self.norm2(x + self.dropout(ff))
+    def forward(self, x, mask):
+
+        att_output = self.masked_multi_att(x, mask)
+        x = self.norm1(x + self.dropout(att_output))
+
+        att_output = self.multi_att(x, mask)
+        x = self.norm2(x + self.dropout(att_output))
+
+        ff_out = self.feedforward(x)
+        x = self.norm3(x + self.dropout(att_output))
+
+        return x 
     
 
+class TransformerDecoder(nn.Module): 
+
+    def __init__(self, d_model, num_head, d_ff, dropout, vocab_size, max_seq_length, num_layers):
+        super(TransformerDecoder, self).__init__()
+        self.output_embedding = nn.Embedding(vocab_size, d_model)
+        self.positional_embedding = PositionalEncoding(d_model, max_seq_length)
+        self.decoders = nn.ModuleList([DecoderLayer(d_model, num_head, d_ff, dropout) for _ in range(num_layers)])
+        self.linear = nn.Linear(d_model, vocab_size)
 
 
-class TransformerEncoder(nn.Module): 
+    def forward(self, x, mask): 
+        
 
-    def __init__(self): 
-        pass 
+        x = self.output_embedding(x)
+        x = self.positional_embedding(x)
 
+        for layer in self.decoders:
+            x = layer(x, mask)
 
-    def forward(self): 
-        pass 
+        return x 
+
---
+++ deepmodel/transformer/blocks/encoder.py
@@ -0,0 +1,50 @@
+import torch 
+import torch.nn as nn 
+import math 
+from layer.attention import MultiHeadAttention
+from layer.pointwise_feed_forward import Position_wise_feed_forward
+from embedding.positional_emdbedding import PositionalEncoding
+
+class EncoderLayer(nn.Module): 
+
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super(EncoderLayer, self).__init__()
+
+        self.multi_att = MultiHeadAttention(d_model, num_heads)
+        self.feedforward = Position_wise_feed_forward(d_model, d_ff)
+
+        # setup norm and dropout 
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+
+
+    def forward(self, x, mask ): 
+        att_ouput = self.multi_att(x, mask )
+        x = self.norm1(x + self.dropout(att_ouput))
+        ff = self.feedforward(x)
+        return self.norm2(x + self.dropout(ff))
+    
+
+
+
+class TransformerEncoder(nn.Module): 
+
+    def __init__(self, vocab_size,  d_model, max_seq_length, num_layer, d_ff, dropout, num_heads ): 
+        super(TransformerEncoder, self).__init__()
+
+
+        self.position_encoder = PositionalEncoding(d_model, max_seq_length)
+        self.input_embedding = nn.Embedding(vocab_size, d_model)
+        self.encoder_layer = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout ) for _ in range(num_layer)])
+
+
+    def forward(self, x, mask):
+        x = self.input_embedding(x)
+        x = self.position_encoder(x)
+        
+        for layer in self.encoder_layer: 
+            x = layer(x, mask)
+
+        return x
